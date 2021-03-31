b15019400625b144bb5cc6c8b1f1b3039b6b6b4b
Oscar Harrysson <harrysson.oscar@gmail.com>
2021-03-31 16:35:22 +0200

    Changed the delay between each message and how long a message can be

--- commands/spam.js
+++ commands/spam.js
@@ -5,20 +5,20 @@ module.exports = {
     let loop = parseInt(args, 10);
 
     (function myLoop(i) {
-      let delay = Math.random() * 5 + 0.25;
+      let delay = Math.random() * 3 + 0.5;
 
       let result = "";
       let characters = "abcdefghijklmnopqrstuvwxyz";
       let charactersLength = characters.length;
 
-      let gibberish = Math.floor(Math.random() * 3) + 3;
+      let gibberish = Math.floor(Math.random() * 5) + 1;
 
       for (var j = 0; j < gibberish; j++)
         result += characters.charAt(
           Math.floor(Math.random() * charactersLength)
         );
       setTimeout(function () {
-        message.channel.send(`${result}, ${i}`); //  your code here
+        message.channel.send(`${result}`); //  your code here
         if (--i) myLoop(i); //  decrement i and call myLoop again if i > 0
       }, delay * 1000);
     })(loop);
