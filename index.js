d3bf67eacc7e19d6ee3efc9b5343bb9a7eab722c
Oscar Harrysson <harrysson.oscar@gmail.com>
2021-03-31 16:29:51 +0200

    Added version 1.0 autocatcher for the bot, using a second prefix and three listening commands

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ for (const file of commandFiles) {
 }
 
 const prefix = "µ-";
+const prefix2 = "The pokémon ";
 
 client.once("ready", async () => {
   console.log(cli.green(`Logged in as ${client.user.username}!`));
@@ -42,4 +43,38 @@ client.on("message", async (message) => {
   }
 });
 
-client.login(process.env.TOKEN);
+client.on("message", async (message) => {
+  if (message.embeds[0]) {
+    for (let embed of message.embeds) {
+      if (embed.title === "A wild pokémon has appeared!") {
+        return message.channel.send(`.h`);
+      } else return;
+    }
+  } else return;
+});
+
+client.on("message", (message) => {
+  if (message.content === "That is the wrong pokémon!") {
+    setTimeout(function () {
+      return message.channel.send(".h");
+    }, 10000);
+  }
+});
+
+client.on("message", async (message) => {
+  if (!message.content.startsWith(prefix2)) return;
+  if (
+    message.author.id === "716390085896962058" &&
+    (message.channel.id === "826377294519861275" ||
+      message.channel.id === "826535927336796180")
+  ) {
+    const args = message.content.slice(prefix2.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === "is") {
+      client.commands.get("autocatcher").execute(message, args);
+    }
+  }
+});
+
+client.login(process.env.TOKEN2);
